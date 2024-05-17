eb0777006454639ed77d13aff54d6dabc7544fd3
godzu2208 <lel54879@gmail.com>
2024-05-17 14:38:28 +0700

    update time 2

---
+++ src/actions/action.js
@@ -0,0 +1,23 @@
+
+import { INCREMENT, DECREMENT } from './types.js';
+
+
+export const increaseCounter = () => {
+
+    return {
+
+        type: INCREMENT,
+
+    };
+
+};
+
+export const decreaseCounter = () => {
+
+    return {
+
+        type: DECREMENT,
+
+    };
+
+};
